4984b0198fa1e55e7eac887b2525f4c863a95160
Neo Prohnitchi <194150828+proneo14@users.noreply.github.com>
2026-09-03 00:35:33 -0400

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        maxdiff = 0
+        minp = prices[0]
+
+        for i in range(len(prices)):
+            maxp = prices[i]
+            
+            if (maxp - minp) > maxdiff:
+                maxdiff = maxp - minp
+            
+            if prices[i] < minp:
+                minp = prices[i]
+
+        return maxdiff
